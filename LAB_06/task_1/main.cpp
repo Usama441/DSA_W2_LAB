54b5ae7c47784f726ceb36f3f934306da52cac93
Muhammad Usama Arshad <112264176+Usama441@users.noreply.github.com>
2024-11-05 18:29:54 +0000

    lab_06 added

---
+++ LAB_06/task_1/main.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+
+using namespace std;
+
+int main(){
+
+    cout <<"nothing edit"<<endl;
+    cout << "nothing added"<<endl;
+    cout <<"nothing edit1"<<endl;
+    cout << "nothing added1"<<endl;
+
+}
