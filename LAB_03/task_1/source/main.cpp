38d77bbc81a1c03886a3033f7200f7639499f833
Muhammad Usama Arshad <112264176+Usama441@users.noreply.github.com>
2024-11-06 17:44:17 +0000

    Lab_03 task1 added

---
+++ LAB_03/task_1/source/main.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+
+using namespace std;
+
+
+int main(){
+   
+   int size = 10;
+
+   int arr[size];
+
+   for(int i = 0 ; i<size ; i++ ){
+     
+     arr[i] = i;
+   }
+  
+    for(int i = 0 ; i<size ; i++ ){
+     
+     cout << arr[i] << endl;
+   }
+}
+
+/*Array Declaration: Write the C++ code to declare 
+an array of 10 integers and initialize it with values from 1 to 10.*/
